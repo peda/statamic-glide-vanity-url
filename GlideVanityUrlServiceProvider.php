558155bc3ec048339f93df3b5d6782fcfbceb992
Peter Rainer <rainer.peter@gmail.com>
2017-10-31 23:22:33 +0100

    updated to be compatible with statamic 2.7.1
    * added route for image URLs (previously was part of statamic core)
    * updated GlideUrlBuilder
    * set default image quality to 85 (if not set) instead of the statamic default of 90 (to improve image size)

--- GlideVanityUrlServiceProvider.php
+++ GlideVanityUrlServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Statamic\Addons\GlideVanityUrl;
+namespace Statamic\Addons\GlideDefaultQuality;
 
 use Statamic\Extend\ServiceProvider;
 use Statamic\API\File;
@@ -23,10 +23,42 @@ use Statamic\API\URL;
 use League\Glide\Urls\UrlBuilderFactory;
 use Statamic\Imaging\ImageUrlBuilder;
 use Statamic\Providers\GlideServiceProvider;
+use Statamic\Http\Controllers\GlideController;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\RouteCollection;
 
 class GlideVanityUrlServiceProvider extends GlideServiceProvider
 {
     public $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Here we get all the current routes, then add our sitemap route right before the catch-all segment route
+        $old_routes = app('router')->getRoutes();
+        $new_routes = new RouteCollection();
+
+        foreach ($old_routes as $i => $route) {
+            if ($route->getUri() == '{segments?}') {
+
+                $imgRoute = new Route(
+                    ['GET'],
+                    Config::get('assets.image_manipulation_route').'/asset/{container}/{path}/{filename?}',
+                    ['uses' => 'GlideController@generateByAsset']
+                );
+
+                $new_routes->add($imgRoute);
+            }
+
+            $new_routes->add($route);
+        }
+
+        app('router')->setRoutes($new_routes);
+    }
 
     public function register()
     {
@@ -57,11 +89,6 @@ class GlideVanityUrlServiceProvider extends GlideServiceProvider
 class GlideVanityUrlBuilder extends GlideUrlBuilder
 {
     /**
-     * @var array
-     */
-    protected $options;
-
-    /**
      * @param array $options
      */
     public function __construct(array $options = [])
@@ -80,23 +107,24 @@ class GlideVanityUrlBuilder extends GlideUrlBuilder
      */
     public function build($item, $params, $filename = null)
     {
+        if(!array_has($params, 'q')) {
+            $params['q'] = '85';
+        }
+
         $this->item = $item;
 
         switch ($this->itemType()) {
-            case 'asset':
-                $path = 'id/' . $this->item->id() . '/' . $this->item->basename();
-                break;
-            case 'id':
-                $asset = ($this->item instanceof Asset)
-                    ? $this->item
-                    : AssetAPI::find($this->item);
-                $path = 'id/' . $this->item . '/' . $asset->basename();
-                break;
             case 'url':
                 $path = 'http/' . base64_encode($item);
                 break;
+            case 'asset':
+                $path = 'asset/' . base64_encode($this->item->containerId() . '/' . $this->item->path()) . '/' . $this->item->basename();
+                break;
+            case 'id':
+                $path = 'asset/' . base64_encode(str_replace('::', '/', $this->item));
+                break;
             case 'path':
-                $path = $this->item;
+                $path = URL::encode($this->item);
                 break;
             default:
                 throw new Exception("Cannot build a Glide URL without a URL, path, or asset.");
